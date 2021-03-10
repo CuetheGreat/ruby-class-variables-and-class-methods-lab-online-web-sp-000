1cc08eeb211d1958ff1edc2fc5f574d91fedf6b0
Clayon Stewart <c.stewart89@gmail.com>
2021-03-10 23:45:17 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,51 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+   @@artist_count = {}
+  
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    if @@genres.find? { |i| i == genre}
+      @@genre_count[genre] += 1
+    else
+      @@genres << genre
+      @@genre_count[genre] = 0
+    end
+    
+    if @@artists.find? { |i| i == artist}
+      @@artist_count[genre] += 1
+    else
+      @@artists << artist
+      @@artist_count[artist] = 0
+    end
+      
+  end
+  
+  def self.count
+    count
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genre_count
+    @@genre_count
+  end
+  
+  def self.artist_count
+    @@genre_count
+  end
+  
+end
