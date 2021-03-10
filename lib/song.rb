a7bf2dc900d202f911caa8f8bef3e43d413d5eda
Clayon Stewart <c.stewart89@gmail.com>
2021-03-10 23:45:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,13 +5,14 @@ class Song
   @@genres = []
   @@artists = []
   @@genre_count = {}
-   @@artist_count = {}
+  @@artist_count = {}
   
   def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    
     if @@genres.find? { |i| i == genre}
       @@genre_count[genre] += 1
     else
@@ -25,7 +26,6 @@ class Song
       @@artists << artist
       @@artist_count[artist] = 0
     end
-      
   end
   
   def self.count
